cfeda3c6267fe268efd9d6147cf6d677f90834af
ABBUGARI POOJITHA <poojithaabbugari041@gmail.com>
2025-10-06 12:25:26 -0300

    Add files via upload

---
+++ ar.py
@@ -0,0 +1,38 @@
+#1.Arithmetic Operators
+#Addition
+
+#(i)Addition,  adds the values of variables gives the output 14
+#s = 4
+#h = 10
+#print(s + h)
+
+#(ii) Subtraction, substracts the values of variables,and  it prints 4
+'''r = 10
+a = 6
+print(r - a) '''
+
+#(iii) Multiplication, it multiplies the values and prints the final(multiplies values) output 44
+'''v = 22
+y = 2
+print(v * y)'''
+
+#(iv) Division, it performs the division and prints the output(quotient) 9.0
+a = 18
+b = 2
+print(a/b)
+
+#(v) Modulus , it prints the remainder as 1
+'''p = 22
+o = 3
+print(p % o)'''
+
+#(vi) Floor Division, it print the munber (like if we do division we get remainder as 5.3,but floor division will print only 5 not 5.33333) 
+'''o = 16
+j = 3
+print(o // j) '''
+
+#(vii) Exponentiation, it calculates the power,2 power 5(2**5) is 32
+'''a = 2
+A = 5
+print(a ** A)'''
+
